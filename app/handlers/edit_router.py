2b9b23d2cf9010e60a8c0d9976be676ce688592a
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-09-03 16:21:04 +0300

    feat: add file size check when editing

--- app/handlers/edit_router.py
+++ app/handlers/edit_router.py
@@ -10,7 +10,7 @@ from app.fsm import MenuState
 from app.keyboards import make_buttons, make_keyboard
 from app.utils.file_utils import get_document_type, get_file_type
 from app.utils.lang_utils import _, __
-from config import BOT_NAME, TTL, WEB_APP_NAME
+from config import BOT_NAME, MAX_FILE_SIZE_BYTES, TTL, WEB_APP_NAME
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -29,15 +29,21 @@ async def handle_edit_start(message: Message, state: FSMContext):
 @router.message(MenuState.on_edit_start, DocumentTypeFilter())
 async def handle_edit_document_upload(message: Message, state: FSMContext, r: Redis):
     try:
-        key = f"{message.chat.id}:{message.document.file_unique_id}"
+        file = message.document
+        if file.file_size > MAX_FILE_SIZE_BYTES:
+            await message.answer(
+                _("The file is too large. Maximum allowed file size is 20 MB")
+            )
+
+        key = f"{message.chat.id}:{file.file_unique_id}"
 
         pipeline = r.pipeline()
         config_data = {
-            "file_id": message.document.file_id,
-            "file_unique_id": message.document.file_unique_id,
-            "file_name": message.document.file_name,
-            "document_type": get_document_type(message.document.file_name),
-            "file_type": get_file_type(message.document.file_name),
+            "file_id": file.file_id,
+            "file_unique_id": file.file_unique_id,
+            "file_name": file.file_name,
+            "document_type": get_document_type(file.file_name),
+            "file_type": get_file_type(file.file_name),
             "chat_id": message.chat.id,
             "message_id": message.message_id,
         }
@@ -45,7 +51,7 @@ async def handle_edit_document_upload(message: Message, state: FSMContext, r: Re
         pipeline.expire(f"{key}:config", TTL)
         pipeline.execute()
 
-        web_app_url = f"https://t.me/{BOT_NAME}/{WEB_APP_NAME}?startapp={message.chat.id}_{message.document.file_unique_id}"
+        web_app_url = f"https://t.me/{BOT_NAME}/{WEB_APP_NAME}?startapp={message.chat.id}_{file.file_unique_id}"
         link_preview_options = LinkPreviewOptions(url=web_app_url)
 
         await state.clear()

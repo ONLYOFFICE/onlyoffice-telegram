7e2a4dd6b167be93f6469ecfd1792ac0475e2177
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-10-25 10:27:17 +0300

    fix: link on editor only for private chat

--- app/filters/document_edit_filter.py
+++ app/filters/document_edit_filter.py
@@ -7,12 +7,13 @@ from app.utils.lang_utils import _
 
 class DocumentEditFilter(BaseFilter):
     async def __call__(self, message: Message) -> bool:
-        if message.document:
-            format = get_format_by_mime(message.document.mime_type)
-            if format and "edit" in format["actions"]:
-                return {"format": format}
-            else:
+        if message.chat.type == "private":
+            if message.document:
+                format = get_format_by_mime(message.document.mime_type)
+                if format and "edit" in format["actions"]:
+                    return {"format": format}
+                else:
+                    return await message.answer(_("File not supported"))
+            elif message.photo:
                 return await message.answer(_("File not supported"))
-        elif message.photo:
-            return await message.answer(_("File not supported"))
         return False

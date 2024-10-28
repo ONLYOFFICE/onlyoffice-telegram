bd8bb54e052a87ec2d523f6defcb9135973698b4
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-10-28 15:08:34 +0300

    feat: update locales

--- app/description/description.py
+++ app/description/description.py
@@ -7,13 +7,15 @@ async def set_description(bot: Bot):
     for lang in i18n.available_locales:
         await bot.set_my_description(
             _(
-                "📑 ONLYOFFICE bot can create, open and convert office files\n\n- Create documents, spreadsheets, presentations, PDF forms\n- Open your local files and edit them\n- Collaborate with other people in real time\n- Convert files to multiple formats\n\nPress /start to create/edit files\n"
+                "📑 ONLYOFFICE bot can create, open and convert office files\n\n- Create documents, spreadsheets, presentations, PDF forms\n- Open your local files and edit them\n- Collaborate with other people in real time\n- Convert files to multiple formats\n\nPress /start to create/edit files\n",
+                locale=lang,
             ),
             language_code=lang,
         )
         await bot.set_my_short_description(
             _(
-                "Easily create, convert, edit and collaborate on office files using the ONLYOFFICE bot."
+                "Easily create, convert, edit and collaborate on office files using the ONLYOFFICE bot.",
+                locale=lang,
             ),
             language_code=lang,
         )

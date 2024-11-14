801946ec4875ecd37154c4be2ba9af9e6ef0ed5c
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-11-14 10:56:51 +0300

    feat: keyboard persistent

--- app/keyboards/make_keyboard.py
+++ app/keyboards/make_keyboard.py
@@ -2,4 +2,4 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 
 def make_keyboard(keyboard: list[list[KeyboardButton]]) -> ReplyKeyboardMarkup:
-    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True, is_persistent=True)

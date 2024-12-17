ad80afd541d2436c4d03b29159646f42d56ca642
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-12-17 14:02:09 +0300

    fix: dot in filename

--- app/filters/document_name_filter.py
+++ app/filters/document_name_filter.py
@@ -22,7 +22,7 @@ from aiogram.types import Message
 
 class DocumentNameFilter(BaseFilter):
     async def __call__(self, message: Message) -> bool:
-        forbidden_chars = r'[<>:"/\\|?*]'
+        forbidden_chars = r'[<>:"/\\|?*\.]'
         if message.text:
             if re.search(forbidden_chars, message.text):
                 return False
